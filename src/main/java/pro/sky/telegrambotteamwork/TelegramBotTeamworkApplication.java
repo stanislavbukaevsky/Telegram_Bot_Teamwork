1f70107d67a3d0f89a680c015429e1bdeec4801f
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-02-19 13:24:49 +0300

    Ветка dev

--- src/main/java/pro/sky/telegrambotteamwork/TelegramBotTeamworkApplication.java
+++ src/main/java/pro/sky/telegrambotteamwork/TelegramBotTeamworkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TelegramBotTeamworkApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TelegramBotTeamworkApplication.class, args);
 	}
 
