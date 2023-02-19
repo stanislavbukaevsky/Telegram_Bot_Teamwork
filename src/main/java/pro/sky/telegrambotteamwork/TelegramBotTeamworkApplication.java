50af38fb9cd1ec3305801c74ac3d1dc9e7457b77
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-02-19 13:10:23 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/telegrambotteamwork/TelegramBotTeamworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambotteamwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotTeamworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotTeamworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/telegrambotteamwork/TelegramBotTeamworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambotteamwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotTeamworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
