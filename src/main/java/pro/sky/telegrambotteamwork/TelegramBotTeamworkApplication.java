b602f6598d75d7ff6a60383b371b621bf930cfcf
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-02-21 19:13:16 +0300

    Реакция на команду /start и добавление кнопок

--- src/main/java/pro/sky/telegrambotteamwork/TelegramBotTeamworkApplication.java
+++ src/main/java/pro/sky/telegrambotteamwork/TelegramBotTeamworkApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TelegramBotTeamworkApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(TelegramBotTeamworkApplication.class, args);
 	}
 
---
+++ src/main/java/pro/sky/telegrambotteamwork/configurations/TelegramBotConfiguration.java
@@ -0,0 +1,22 @@
+package pro.sky.telegrambotteamwork.configurations;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.DeleteMyCommands;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfiguration {
+    // Поле токена для телеграм-бота
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    // Создание нового бота
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
+    }
+}
