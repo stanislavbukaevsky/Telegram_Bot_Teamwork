9d70a6b4eeae317b0db5cacaa3dd47a137d57981
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-03-03 19:52:51 +0300

    Сохранение пользователя в базу данных

--- src/main/java/pro/sky/telegrambotteamwork/listeners/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambotteamwork/listeners/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambotteamwork.listeners;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
@@ -10,10 +11,9 @@ import com.pengrad.telegrambot.response.SendResponse;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambotteamwork.model.User;
-import pro.sky.telegrambotteamwork.repositories.UserRepository;
+import pro.sky.telegrambotteamwork.repository.UserRepository;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -51,10 +51,10 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Запрос от пользователя: {}", update);
             String message = update.message().text();
+            Message messageUser = update.message();
+            update.message().entities();
             long chatId = update.message().chat().id();
             String name = update.message().chat().firstName();
-            long userId = update.message().from().id();
-//            String phoneNumber = update.message().contact().phoneNumber();
 
             switch (message) {
                 case START: {
@@ -65,11 +65,47 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
                     SendMessage sendMessage = new SendMessage(chatId, "Выберите из списка, что вы хотите узнать.")
                             .replyMarkup(new ReplyKeyboardMarkup(new String[][]{
                                     {INFORMATION_ABOUT_THE_SHELTER, TAKE_A_PET_FROM_A_SHELTER},
-                                    {PET_REPORT, CALL_A_VOLUNTEER}
+                                    {PET_REPORT, CALL_A_VOLUNTEER},
+                                    {"Подписаться"}
                             }).resizeKeyboard(true));
                     telegramBot.execute(sendMessage);
+
+//                        Keyboard keyboard = new ReplyKeyboardMarkup(
+//                                new KeyboardButton[]{
+//                                        new KeyboardButton("text"),
+//                                        new KeyboardButton("Отправить контакт").requestContact(true)
+//                                }
+//                        ).resizeKeyboard(true);
+//                        SendMessage sendMessage = new SendMessage(chatId, "Выберите из списка, что вы хотите узнать")
+//                                .replyMarkup(keyboard);
+//                        telegramBot.execute(sendMessage);
+
+//                        InlineKeyboardMarkup inlineKeyboardMarkupOne = new InlineKeyboardMarkup(
+//                                new InlineKeyboardButton(INFORMATION_ABOUT_THE_SHELTER).callbackData("Здесь будет информация о приюте")
+//                        );
+//                        InlineKeyboardMarkup inlineKeyboardMarkupTwo = new InlineKeyboardMarkup(
+//                                new InlineKeyboardButton(TAKE_A_PET_FROM_A_SHELTER).callbackData("Здесь будет информация как взять питомца из приюта")
+//                        );
+//                        InlineKeyboardMarkup inlineKeyboardMarkupThree = new InlineKeyboardMarkup(
+//                                new InlineKeyboardButton(PET_REPORT).callbackData("Здесь будет информация о том, как прислать отчет о питомце")
+//                        );
+//                        InlineKeyboardMarkup inlineKeyboardMarkupFour = new InlineKeyboardMarkup(
+//                                new InlineKeyboardButton(CALL_A_VOLUNTEER).callbackData("Здесь будет информация о том, как позвать волонтера")
+//                        );
+//                        SendMessage sendMessage = new SendMessage(chatId, "Выберите из списка, что вы хотите узнать")
+//                                .replyMarkup(inlineKeyboardMarkupOne)
+//                                .replyMarkup(inlineKeyboardMarkupTwo)
+//                                .replyMarkup(inlineKeyboardMarkupThree)
+//                                .replyMarkup(inlineKeyboardMarkupFour);
+//                        telegramBot.execute(sendMessage);
                     break;
                 }
+                case "Подписаться": {
+                    sendMessage(chatId, "Пользователь сохранен");
+                    saveUser(messageUser);
+                    break;
+                }
+
                 case INFORMATION_ABOUT_THE_SHELTER: {
                     sendMessage(chatId, "Здесь будет информация о приюте");
                     break;
@@ -94,14 +130,18 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     /**
      * Метод, который сохраняет пользователя в базу данных
      *
-     * @param name   Имя пользователя
-     * @param userId Идентификатор пользователя
+     * @param message Пользовательские данные юзера
      */
-    private void saveUser(String name, long userId) {
+    private void saveUser(Message message) {
+        String name = message.chat().firstName();
+        Long userId = message.from().id();
+        String phone = message.contact().phoneNumber();
         User user = new User();
         user.setUserName(name);
         user.setUserId(userId);
+        user.setPhone(phone);
         userRepository.save(user);
+        logger.info("Ползователь сохранен в базе полностью: {}", user);
     }
 
     private void sendMessage(long chatId, String message) {
--- src/main/resources/liquibase/scripts/bot.sql
+++ src/main/resources/liquibase/scripts/bot.sql
@@ -9,21 +9,3 @@ CREATE TABLE notification_task
     timeDate CHARACTER
 );
 
---changeset sbukaevsky:2
-CREATE TABLE users
-(
-    id        BIGINT,
-    user_name TEXT,
-    user_id   BIGINT,
-    phone     TEXT
-);
-
---changeset olgakargan:3
-CREATE TABLE pet
-(
-    id              BIGINT,
-    breed           TEXT,
-    pet_name        TEXT,
-    year_of_birth   INT,
-    description     TEXT
-)
